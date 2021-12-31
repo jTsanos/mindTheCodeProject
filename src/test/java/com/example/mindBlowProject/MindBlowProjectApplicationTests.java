3ac1fd4ec29565dd7f0636554e723444845170ec
dkokas <dkokas>
2021-12-31 20:03:51 +0200

    //Steps connecting MongoDB// Run Docker_compose successfully and:
    ---> If you Have intelliJ ultimate:
    Right toolbar-Database-->add Data Source (Mongo DB)
    -------CREDENTIALS---------
    username:rootuser
    password:rootuser
    DBname:omadapiravlos
    Ensure test connection is ok & apply
    
    --->If you Have NOT ultimate edition:
    Go to localhost:8081 and create a new database named:
    omadapiravlos
    
    build & run main
    
    and... '30000ms' left in 2021 :)
    
    πείραξα και λίγο HomeWebController με Thymeleaf, ανεβάζω λιγες αλλαγές όμως, τα υπόλοιπα στο επόμενο call!
    Καλή Χρονιά Παίδες!

--- src/test/java/com/example/mindBlowProject/MindBlowProjectApplicationTests.java
+++ src/test/java/com/example/mindBlowProject/MindBlowProjectApplicationTests.java
@@ -1,5 +1,5 @@
-package com.example.mindBlowProject;
-
+/* package com.example.mindBlowProject;
+<!--
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +10,4 @@ class MindBlowProjectApplicationTests {
 	void contextLoads() {
 	}
 
-}
+} --!>*/
