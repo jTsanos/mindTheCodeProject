c12b43aa8c56e843f8d16cd78c92c271b8b5efb3
dkokas <dkokas>
2021-12-31 20:03:51 +0200

    //Steps connecting MongoDB// Run Docker_compose successfully and:
    ---> If you Have intelliJ ultimate:
    Right toolbar-Database-->add Data Source (Mongo DB)
    -------CREDENTIALS---------
    username:useroot
    password:useroot
    DBname:omadapiravlos
    Ensure test connection is ok & apply
    
    --->If you Have NOT ultimate edition:
    Go to localhost:8081 and create a new database named:
    omadapiravlos
    
    build & run main
    
    and... '30000ms' left in 2021 :)
    
    πείραξα και λίγο HomeWebController με Thymeleaf, ανεβάζω λιγες αλλαγές όμως, τα υπόλοιπα στο επόμενο call!
    Καλή Χρονιά Παίδες!

---
+++ src/main/java/com/example/mindBlowProject/mvc/HomeWebC.java
@@ -0,0 +1,14 @@
+package com.example.mindBlowProject.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeWebC {                    /*  Mapping host:port/ --to--> templates/landing_page */
+    @GetMapping("/")
+        public String home(){
+        return "landing_page";
+    }
+
+
+}
